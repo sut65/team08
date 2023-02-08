9495a6ab931b694de4b583c4b607d9e8b890ec81
B6332907 <sameaw13@gmail.com>
2023-02-08 22:50:36 +0700

    update - close #16

--- backend/controller/Screening_officer.go
+++ backend/controller/Screening_officer.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"github.com/asaskevich/govalidator"
 	"github.com/sut65/team08/entity"
 
 	"github.com/gin-gonic/gin"
@@ -9,91 +10,92 @@ import (
 )
 
 // // POST /Screening_officer
-// func CreateScreening_officer(c *gin.Context) {
-// 	var officer entity.Officer
+func CreateScreening_officer(c *gin.Context) {
+	var officer entity.Officer
+	var screening_officer entity.Screening_officer
+	var Prefix entity.Prefix
+	var gender entity.Gender
+	var blood entity.Blood
+	var religion entity.Religion
+	var nationality entity.Nationality
+	var education entity.Education
 
-// 	var screening_officer entity.Screening_officer
-// 	var Prefix entity.Prefix
-// 	var gender entity.Gender
-// 	var blood entity.Blood
-// 	var religion entity.Religion
-// 	var nationality entity.Nationality
-// 	var education entity.Education
+	if err := c.ShouldBindJSON(&screening_officer); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-// 	// ผลลัพธ์ที่ได้จากขั้นตอนที่ 8 จะถูก bind เข้าตัวแปร Screening_officer
-// 	if err := c.ShouldBindJSON(&screening_officer); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
+	// 10: ค้นหา prefix ด้วย id
+	if tx := entity.DB().Where("id = ?", screening_officer.PrefixID).First(&Prefix); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Prefix not found"})
+		return
+	}
 
-// 	// 10: ค้นหา prefix ด้วย id
-// 	if tx := entity.DB().Where("id = ?", screening_officer.PrefixID).First(&Prefix); tx.RowsAffected == 0 {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Prefix not found"})
-// 		return
-// 	}
+	// 11: ค้นหา Gender ด้วย id
+	if tx := entity.DB().Where("id = ?", screening_officer.GenderID).First(&gender); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Gender not found"})
+		return
+	}
 
-// 	// 11: ค้นหา Gender ด้วย id
-// 	if tx := entity.DB().Where("id = ?", screening_officer.GenderID).First(&gender); tx.RowsAffected == 0 {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Gender not found"})
-// 		return
-// 	}
+	// 12: ค้นหา Education ด้วย id
+	if tx := entity.DB().Where("id = ?", screening_officer.EducationID).First(&education); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "education not found"})
+		return
+	}
+	// 13: ค้นหา blood ด้วย id
+	if tx := entity.DB().Where("id = ?", screening_officer.BloodID).First(&blood); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "blood not found"})
+		return
+	}
+	// 14: ค้นหา religion ด้วย id
+	if tx := entity.DB().Where("id = ?", screening_officer.ReligionID).First(&religion); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "religion not found"})
+		return
+	}
+	// 15: ค้นหา religion ด้วย id
+	if tx := entity.DB().Where("id = ?", screening_officer.NationalityID).First(&nationality); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "nationality not found"})
+		return
+	}
 
-// 	// 12: ค้นหา Education ด้วย id
-// 	if tx := entity.DB().Where("id = ?", screening_officer.EducationID).First(&education); tx.RowsAffected == 0 {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "education not found"})
-// 		return
-// 	}
-// 	// 13: ค้นหา blood ด้วย id
-// 	if tx := entity.DB().Where("id = ?", screening_officer.BloodID).First(&blood); tx.RowsAffected == 0 {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "blood not found"})
-// 		return
-// 	}
-// 	// 14: ค้นหา religion ด้วย id
-// 	if tx := entity.DB().Where("id = ?", screening_officer.ReligionID).First(&religion); tx.RowsAffected == 0 {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "religion not found"})
-// 		return
-// 	}
-// 	// 15: ค้นหา religion ด้วย id
-// 	if tx := entity.DB().Where("id = ?", screening_officer.NationalityID).First(&nationality); tx.RowsAffected == 0 {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "nationality not found"})
-// 		return
-// 	}
+	//  ค้น OfficerID
+	if tx := entity.DB().Where("id = ?", screening_officer.OfficerID).First(&officer); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "officer not found"})
+		return
+	}
 
-// 	//  ค้น OfficerID
-// 	if tx := entity.DB().Where("id = ?", screening_officer.OfficerID).First(&officer); tx.RowsAffected == 0 {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "officer not found"})
-// 		return
-// 	}
+	// 	// 16: สร้าง Screening_officer
+	sc := entity.Screening_officer{
+		Prefix:                 screening_officer.Prefix,
+		Screening_officer_Name: screening_officer.Screening_officer_Name,
+		GenderID:               screening_officer.GenderID,
+		BloodID:                screening_officer.BloodID,
+		ReligionID:             screening_officer.ReligionID,
+		Birthday:               screening_officer.Birthday,
+		NationalityID:          screening_officer.NationalityID,
+		ScreeningIDCard:        screening_officer.ScreeningIDCard,
+		Phone:                  screening_officer.Phone,
+		Email:                  screening_officer.Email,
+		EducationID:            screening_officer.EducationID,
+		EducationName:          screening_officer.EducationName,
+		EducationMajor:         screening_officer.EducationMajor,
+		University:             screening_officer.University,
 
-// 	// 16: สร้าง Screening_officer
-// 	sc := entity.Screening_officer{
-// 		Prefix:                 Prefix,
-// 		Screening_officer_Name: screening_officer.Screening_officer_Name,
-// 		Gender:                 gender,
-// 		Blood:                  blood,
-// 		Religion:               religion,
-// 		Birthday:               screening_officer.Birthday,
-// 		Nationality:            nationality,
-// 		Country:                nationality,
-// 		ScreeningIDCard:        screening_officer.ScreeningIDCard,
-// 		Phone:                  screening_officer.Phone,
-// 		Email:                  screening_officer.Email,
+		OfficerID: screening_officer.OfficerID,
+	}
 
-// 		Education:      education,
-// 		EducationName:  screening_officer.EducationName,
-// 		EducationMajor: screening_officer.EducationMajor,
-// 		University:     screening_officer.University,
-
-// 		Officer: officer,
-// 	}
-
-// 	// 13: บันทึก
-// 	if err := entity.DB().Create(&sc).Error; err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-// 	c.JSON(http.StatusCreated, gin.H{"data": sc})
-// }
+	//ขั้นตอนการ validate ที่นำมาจาก  unit test
+	if _, err := govalidator.ValidateStruct(sc); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	// 13: บันทึก
+	if err := entity.DB().Create(&sc).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"data": sc})
+}
 func GetScreening_officer(c *gin.Context) {
 	var screening_officer entity.Screening_officer
 	id := c.Param("id")
@@ -103,6 +105,7 @@ func GetScreening_officer(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, gin.H{"data": screening_officer})
 }
+
 // // GET /Screening_officer/:id
 // func GetScreening_officer(c *gin.Context) {
 // 	var screening_officer entity.Screening_officer

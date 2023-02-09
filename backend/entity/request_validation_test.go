e2c61b2b6fb5a66661e4d4d25b15dac5ceb49a97
B6321451 <khunjirapantuket@gmail.com>
2023-02-09 16:51:01 +0700

    validate Date - close #65

--- backend/entity/request_validation_test.go
+++ backend/entity/request_validation_test.go
@@ -74,3 +74,50 @@ func TestRequest_QUANTITY(t *testing.T) {
 
 	g.Expect(err.Error()).To(gomega.Equal("QUANTITY: 2001 does not validate as range(1|2000)"))
 }
+
+//////4
+func TestRequest_DATE(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	// เช็คข้อมูล DATE จะต้องไม่เป็นอดีต
+	Request := Request{
+		R_ID:     "R123456",
+		R_NAME:   "ตรวจเม็ดเลือดขาว",
+		QUANTITY: 1500,
+		TIME:     time.Date(2021, 1, 1, 12, 00, 00, 00, time.UTC),
+	}
+
+	// ตรวจสอบด้วย govalidator
+	ok, err := govalidator.ValidateStruct(Request)
+
+	// ok ต้องเป็น true แปลว่าไม่มี error
+	g.Expect(ok).ToNot(gomega.BeTrue())
+
+	// err ต้องเป็น nil แปลว่าไม่มี error
+	g.Expect(err).ToNot(gomega.BeNil())
+
+	g.Expect(err.Error()).To(gomega.Equal("Please enter the current time"))
+}
+////////5
+func TestRequest_DATETREATMENT(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	// เช็คข้อมูล DATE จะต้องไม่เป็นอดีต
+	Request := Request{
+		R_ID:     "R100002",
+		R_NAME:   "ตรวจเลือดดดดดดดดดดดดดดดดดดดดดดดด",
+		QUANTITY: 800,
+		TIME:     time.Now(),
+	}
+
+	// ตรวจสอบด้วย govalidator
+	ok, err := govalidator.ValidateStruct(Request)
+
+	// ok ต้องเป็น true แปลว่าไม่มี error
+	g.Expect(ok).ToNot(gomega.BeTrue())
+
+	// err ต้องเป็น nil แปลว่าไม่มี error
+	g.Expect(err).ToNot(gomega.BeNil())
+
+	g.Expect(err.Error()).To(gomega.Equal("Please enter details"))
+}

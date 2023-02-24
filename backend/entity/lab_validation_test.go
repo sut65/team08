0e4e3f94062ba5965246466d3b8baab989c0c167
idearslove <idea.bunyalit@gmail.com>
2023-02-24 18:12:59 +0700

    add validate lab is value not spacial character - close #72

--- backend/entity/lab_validation_test.go
+++ backend/entity/lab_validation_test.go
@@ -33,3 +33,28 @@ func TestValueMaxcharector15(t *testing.T) {
     g.Expect(err).NotTo(BeNil())
     g.Expect(err.Error()).To(Equal("สามารถกรอกค่าแลปได้สูงสุด 15 ตัวอักษรเท่านั้น"))
 }
+
+func TestValueNotSpecialCharacters(t *testing.T) {
+    g := NewGomegaWithT(t)
+
+    fixtures := []string{
+        "pl",
+        "3.6po",
+        "1.1#",
+        "$#",
+        "xxxxxxxxxx",
+    }
+
+    for _, fixture := range fixtures {
+
+        user := Lab{
+            Lab_test: "negative",
+            Value:    fixture,
+        }
+
+        ok, err := govalidator.ValidateStruct(user)
+        g.Expect(ok).ToNot(BeTrue())
+        g.Expect(err).ToNot(BeNil())
+        g.Expect(err.Error()).To(Equal("กรุณากรอกเป็นตัวเลขทศนิยม"))
+    }
+}

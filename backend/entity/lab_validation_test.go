bec458ad9ba7226b768f47749b8d74e463272ac7
idearslove <idea.bunyalit@gmail.com>
2023-02-24 18:08:33 +0700

    value not many 15 character - close #71

--- backend/entity/lab_validation_test.go
+++ backend/entity/lab_validation_test.go
@@ -20,3 +20,16 @@ func TestLabNotBlank(t *testing.T) {
 	g.Expect(err.Error()).To(Equal("กรุณากรอกค่ารายงานผล"))
 }
 
+func TestValueMaxcharector15(t *testing.T) {
+    g := NewGomegaWithT(t)
+
+    user := Lab{
+        Lab_test: "negative",
+        Value:    "234349483454596.4845949649099009090032",
+    }
+
+    ok, err := govalidator.ValidateStruct(user)
+    g.Expect(ok).NotTo(BeTrue())
+    g.Expect(err).NotTo(BeNil())
+    g.Expect(err.Error()).To(Equal("สามารถกรอกค่าแลปได้สูงสุด 15 ตัวอักษรเท่านั้น"))
+}
